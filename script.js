06665a275e7531590d5b2464ebae43de9b993ea4
rererob886-create <rererob886@gmail.com>
2026-03-04 04:36:43 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const mobileMenu = document.getElementById("mobile-menu");
+const navLinks = document.getElementById("navLinks");
+
+mobileMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll("#navLinks a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
