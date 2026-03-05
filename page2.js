9144f14827030b08d39c99147381858494ce05a2
rererob886-create <rererob886@gmail.com>
2026-03-05 06:03:26 +0300

    Update page2.js

--- page2.js
+++ page2.js
@@ -1,15 +1,25 @@
+const mobileMenu = document.getElementById("mobile-menu");
+const navLinks = document.getElementById("navLinks");
+
+mobileMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll("#navLinks a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
+
 const galleryItems = document.querySelectorAll('.gallery-item img');
 
 galleryItems.forEach(img => {
     img.addEventListener('mouseenter', () => {
-        // تكبير الصورة وإضافة ظل عند المرور
         img.style.transform = 'scale(1.1) rotate(-1deg)';
         img.style.boxShadow = '0 15px 25px rgba(0,0,0,0.35)';
-        img.style.transition = 'all 0.3s ease';
     });
 
     img.addEventListener('mouseleave', () => {
-        // إعادة الصورة لوضعها الطبيعي عند الخروج
         img.style.transform = 'scale(1) rotate(0deg)';
         img.style.boxShadow = '0 8px 25px rgba(0,0,0,0.1)';
     });
